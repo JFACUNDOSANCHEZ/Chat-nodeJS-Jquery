6288dec780265b3e8f708abd6d1a8b5bee56db27
Facu Sanchez <facusanchz96@gmail.com>
2024-03-31 01:32:33 -0300

    COMMIT

--- sockets.js
+++ sockets.js
@@ -1,10 +1,20 @@
-module.exports = function(io){
-
+module.exports = function(io) {
     io.on('connect', socket => {
-        console.log('Nuevo usuario conectado');
-socket.on('send message', function(data){
-io.sockets.emit('new message', data)
-})
+        let username = 'Anónimo';
 
+        // Manejar el evento 'name' enviado por el cliente
+        socket.on('name', function(data) {
+            console.log(data);
+            // Actualizar el nombre de usuario con el nombre recibido del cliente
+            username = data || 'Anónimo';
+            console.log('Nombre recibido:', username);
+        });
+
+        // Manejar el evento 'send message' enviado por el cliente
+        socket.on('send message', function(message) {
+            // Emitir un nuevo evento 'new message' con el nombre de usuario y el mensaje
+            io.emit('new message', { username, message });
+            console.log(`Usuario ${username} ha enviado un mensaje: ${message}`);
+        });
     });
-}
+};
