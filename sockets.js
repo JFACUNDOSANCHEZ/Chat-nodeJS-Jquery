b7737a1d1d9e9b6ec6376940f58b71d75615016a
Facu Sanchez <facusanchz96@gmail.com>
2024-03-31 22:16:20 -0300

    Style

--- sockets.js
+++ sockets.js
@@ -1,12 +1,27 @@
 module.exports = function(io) {
+    const connectedUsers = {}; // Objeto para almacenar usuarios conectados
+
     io.on('connect', socket => {
-        let username = 'Anónimo';
+        let username = '';
+
+        // Asigna un nombre de usuario o genera uno aleatorio si no se proporciona
+        username = socket.handshake.query.username || generateRandomUsername();
+        connectedUsers[socket.id] = username;
+
+        // Envia la lista de usuarios conectados a todos los clientes
+        io.emit('connected users', Object.values(connectedUsers));
+
+        // Maneja la desconexión de un usuario
+        socket.on('disconnect', () => {
+            delete connectedUsers[socket.id];
+            io.emit('connected users', Object.values(connectedUsers));
+        });
 
         // Manejar el evento 'name' enviado por el cliente
         socket.on('name', function(data) {
-            console.log(data);
-            // Actualizar el nombre de usuario con el nombre recibido del cliente
-            username = data || 'Anónimo';
+            username = data || `Anónimo${Math.floor(Math.random() * 1000)}`;
+            connectedUsers[socket.id] = username;
+            io.emit('connected users', Object.values(connectedUsers));
             console.log('Nombre recibido:', username);
         });
 
@@ -16,5 +31,10 @@ module.exports = function(io) {
             io.emit('new message', { username, message });
             console.log(`Usuario ${username} ha enviado un mensaje: ${message}`);
         });
+
+        // Genera un nombre de usuario aleatorio
+        function generateRandomUsername() {
+            return `Anónimo${Math.floor(Math.random() * 1000)}`;
+        }
     });
 };
